a9f04aa7b7ee2f9e934235d7541f5fc4f12df1ef
Caglar <caglarsulun@hotmail.com>
2021-11-23 10:18:40 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+  let headquarter = 42;
+  if (street > headquarter) {
+    return street - headquarter;
+  } else {
+    return headquarter - street;
+  }
+}
+distanceFromHqInBlocks(42);
+
+
+function distanceFromHqInFeet(street) {
+  let headquarter = 42;
+  if (street > headquarter) {
+    return (street - headquarter) * 264;
+  } else if (headquarter >= street) {
+    return (headquarter - street) * 264;
+  }
+}
+distanceFromHqInFeet(50);
+
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264;
+}
+distanceTravelledInFeet(50, 43);
+
+
+function calculatesFarePrice(start, destination){
+    let traveledDistance = Math.abs(start - destination)* 264;
+    if (traveledDistance < 400){
+        return "Free Ride!";
+    }else if (traveledDistance > 400 && traveledDistance <= 2000){
+        return ` Your total is ${((traveledDistance - 400)*2)/100} dollars`;
+    }else if (traveledDistance > 2500){
+        return "cannot travel that far";
+    }else if(traveledDistance > 2000 && traveledDistance <= 2500){
+        return "Flat Rate: 30 dollars"
+    }
+}
