bef67843fa9dae4f70658a9824f66907b9b1c7e0
Caglar <caglarsulun@hotmail.com>
2021-11-23 09:42:21 -0500

    commit tree 11ab098cae91c5fd6a369af86822528a46542b73

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    let headquarter=42;
+    if (street > headquarter) {
+
+        return street - headquarter;  
+
+    } else {
+
+        return headquarter - street;
+    }
+}
+distanceFromHqInBlocks(42);
+
+
+
+function distanceFromHqInFeet(street) {
+    let headquarter = 42;
+    if (street > headquarter){
+        return (street-headquarter)*264;
+    } else if(headquarter >= street){
+        return (headquarter - street)*264;
+    }
+}
+distanceFromHqInFeet(50);
+
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(destination - start)*264;
+}
+
+distanceTravelledInFeet(50,43);
